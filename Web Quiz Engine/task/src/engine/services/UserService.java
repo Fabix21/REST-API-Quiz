4bb019e1669daa29716d1e467e694fb1688eac14
Fabian Hoszowski <fabix43@gmail.com>
2020-07-04 13:46:22 +0200

    Implemented interface UserDetailsService to provide authentication by email.

--- Web Quiz Engine/task/src/engine/services/UserService.java
+++ Web Quiz Engine/task/src/engine/services/UserService.java
@@ -4,13 +4,16 @@ import engine.exceptions.UserEmailTakenException;
 import engine.models.User;
 import engine.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.regex.Pattern;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
@@ -31,5 +34,10 @@ public class UserService {
 
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername( String email ) throws UsernameNotFoundException {
+        return null;
     }
 }
